2fed84fb0f3b092bc04ec701328ee726ce38cd00
Your Name <you@example.com>
2019-05-09 15:24:09 +0300

    Updates

--- precache-manifest.49d832ce0f1f55e4f7f3f6d0c4aeb8b7.js
+++ precache-manifest.49d832ce0f1f55e4f7f3f6d0c4aeb8b7.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/jdjajdjaid/static/js/runtime~main.e4259f13.js"
   },
   {
-    "revision": "75849cc7ac697a58719d",
-    "url": "/jdjajdjaid/static/js/main.75849cc7.chunk.js"
+    "revision": "2ef093fa26d90d26a611",
+    "url": "/jdjajdjaid/static/js/main.2ef093fa.chunk.js"
   },
   {
     "revision": "5fdc4916c413f2382173",
     "url": "/jdjajdjaid/static/js/2.5fdc4916.chunk.js"
   },
   {
-    "revision": "75849cc7ac697a58719d",
+    "revision": "2ef093fa26d90d26a611",
     "url": "/jdjajdjaid/static/css/main.e3a9f038.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/jdjajdjaid/static/css/2.afbad9f7.chunk.css"
   },
   {
-    "revision": "9a8306d7c914bd030f1d3de967c3a0e6",
+    "revision": "ec2a49284a6dd96dc3dd2dde72eb6e90",
     "url": "/jdjajdjaid/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/jdjajdjaid/precache-manifest.88ef6e479b2d546139be8157ae26a893.js"
+  "/jdjajdjaid/precache-manifest.49d832ce0f1f55e4f7f3f6d0c4aeb8b7.js"
 );
 
 workbox.clientsClaim();
