f60f82ff8009b24341553473a3c4f233659bb635
Your Name <you@example.com>
2019-05-26 20:06:29 +0300

    Updates

--- precache-manifest.f8092615af8d4ac3ea3a3f43e2e245f2.js
+++ precache-manifest.f8092615af8d4ac3ea3a3f43e2e245f2.js
@@ -4,6 +4,10 @@ self.__precacheManifest = [
     "url": "/jdjajdjaid/static/media/peterburg.0b7346ca.jpg"
   },
   {
+    "revision": "4475a012258a96b40404ca9b8ac72257",
+    "url": "/jdjajdjaid/static/media/nov.4475a012.jpg"
+  },
+  {
     "revision": "f79645b8ad3b075153483e008f45eff0",
     "url": "/jdjajdjaid/static/media/msk.f79645b8.gif"
   },
@@ -16,19 +20,19 @@ self.__precacheManifest = [
     "url": "/jdjajdjaid/static/js/runtime~main.45e2ac0a.js"
   },
   {
-    "revision": "7b1ca0fc0bfab4a82df3",
-    "url": "/jdjajdjaid/static/js/main.a40c79b2.chunk.js"
+    "revision": "189eceb546e1b14f0733",
+    "url": "/jdjajdjaid/static/js/main.9421ed09.chunk.js"
   },
   {
-    "revision": "a571bea7e6a51a203000",
-    "url": "/jdjajdjaid/static/js/2.b4459d15.chunk.js"
+    "revision": "f20c2a53cffc42ebfb43",
+    "url": "/jdjajdjaid/static/js/2.b74ba5bd.chunk.js"
   },
   {
-    "revision": "a571bea7e6a51a203000",
+    "revision": "f20c2a53cffc42ebfb43",
     "url": "/jdjajdjaid/static/css/2.0e64b50e.chunk.css"
   },
   {
-    "revision": "24803e5564233dde609a9bd8e4b1caa7",
+    "revision": "ae312478e4d5d5115b7e5a81fdba8d44",
     "url": "/jdjajdjaid/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/jdjajdjaid/precache-manifest.60f64ba741424f2401462a09e7e7e00b.js"
+  "/jdjajdjaid/precache-manifest.f8092615af8d4ac3ea3a3f43e2e245f2.js"
 );
 
 workbox.clientsClaim();
